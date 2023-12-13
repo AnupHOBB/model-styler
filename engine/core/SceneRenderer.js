bf5ed87000eabc14574697ba21423f7f04c68f3f
Anup Bhandari <anup.b@HOBB.COM>
2023-12-13 09:49:10 +0530

    Add .nojekyll to support github hosting

--- engine/core/SceneRenderer.js
+++ engine/core/SceneRenderer.js
@@ -218,7 +218,7 @@ export class SceneRenderer
         if (threeJsObject.isLight != undefined)  
             this.addToScene(threeJsObject)
         else
-        {
+        { 
             if (isLuminant)
                 this.bloomObjects.push(threeJsObject)
             else
